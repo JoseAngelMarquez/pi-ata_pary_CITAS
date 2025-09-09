b9eeb63d50e51fce955192068b0af29a389f4478
José Ángel <joseangelmarquezespina060503@gmail.com>
2025-09-09 10:42:06 -0600

    mueve estilos de formcontrol a un css

--- frontend/pages/login.jsx
+++ frontend/pages/login.jsx
@@ -2,28 +2,18 @@ import React from "react";
 import { FormControl } from '@mui/material';
 import { TextField } from '@mui/material';
 import { Button } from '@mui/material';
+import styles from '../styles/login.module.css';
 function Login() {
     return (
         <div>
-            <FormControl
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: "16px",
-                    width: "300px",
-                    padding: "20px",
-                    backgroundColor: "#fff",
-                    borderRadius: "8px",
-                    boxShadow: "0 4px 12px rgba(0,0,0,0.1)"
-                }}
-            >
+            <FormControl className={styles['login-form']} >
 
                 <TextField label="Usuario" variant="outlined" fullWidth style={{ marginBottom: "16px" }} />
                 <TextField label="Contraseña" variant="outlined" fullWidth style={{ marginBottom: "16px" }} />
 
                 <Button variant="contained" color="primary" fullWidth>
-        Iniciar Sesión
-    </Button>
+                    Iniciar Sesión
+                </Button>
 
             </FormControl>
         </div>
