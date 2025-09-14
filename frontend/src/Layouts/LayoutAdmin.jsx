9658225226dea1fcbe6762262de548697af1230f
José Ángel <joseangelmarquezespina060503@gmail.com>
2025-09-14 13:50:54 -0600

    Añade estilos para el Layout

--- frontend/src/Layouts/LayoutAdmin.jsx
+++ frontend/src/Layouts/LayoutAdmin.jsx
@@ -1,30 +1,34 @@
 import React from "react";
 import { MenuList, MenuItem, Paper } from "@mui/material";
 import { Link, Outlet } from "react-router-dom";
-
+import styles from "../styles/LayoutAdmin.module.css"
 function LayoutAdmin() {
   return (
-    <div style={{ display: "flex", height: "100vh" }}>
-    <Paper style={{ width: 200, height: 650 }}>
-      <MenuList>
-        <MenuItem component={Link} to="/admin/Principal">
-          Inicio
-        </MenuItem>
-        <MenuItem component={Link} to="/admin/Principal">
-          pagina1
-        </MenuItem>
-        <MenuItem component={Link} to="/admin/Pagina">
-          pagina
-        </MenuItem>
+    <div className={styles.parent}>
+      <div className={styles.div2}>
+    <h1></h1>
+      </div>
+      <aside className={styles.div1}>
+        <Paper className={styles.paper}>
+          <MenuList>
+            <MenuItem component={Link} to="/admin/Principal">
+              Inicio
+            </MenuItem>
+            <MenuItem component={Link} to="/admin/Principal">
+              pagina1
+            </MenuItem>
+            <MenuItem component={Link} to="/admin/Pagina">
+              pagina
+            </MenuItem>
 
-      </MenuList>
-      </Paper>
-
-      <div style={{ flex: 1, padding: 20, display: "flex", justifyContent: "center", alignItems: "center" }}>
-            <Outlet />
+          </MenuList>
+        </Paper>
+      </aside>
+      <div className={styles.div3}>
+        <Outlet />
       </div>
     </div>
-    
+
   );
 }
 
